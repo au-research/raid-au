8d85ce963c57be442ec76dc902f6a174e41264e4
Abdul <abdul.syed@ardc.edu.au>
2026-03-04 12:51:16 +1100

    megamenu text and icon changes

--- raid-agency-app/src/components/mega-menu/mega-menu.tsx
+++ raid-agency-app/src/components/mega-menu/mega-menu.tsx
@@ -8,7 +8,15 @@ export const MegaMenu = () => {
         <Paper elevation={1} sx={{width: '100%', zIndex: 1200, position: 'relative'}}>
             <Stack direction="row" spacing={2} justifyContent="right" alignItems="center">
                 <Button onClick={() => setToggleBtn(!toggleBtn)}>
-                    <Typography sx={{textTransform: "capitalize", color: "text.primary", display: "contents", alignItems: "center"}}>{("Explore").toLowerCase()} ARDC {toggleBtn ? <ChevronUp  strokeWidth={3} absoluteStrokeWidth style={{marginLeft: "4px", fontWeight: "bold"}} size={20} /> : <ChevronDown  strokeWidth={3} absoluteStrokeWidth style={{marginLeft: "4px"}} size={20} />}</Typography>
+                    <Typography
+                      sx={{
+                        textTransform: "capitalize",
+                        color: "text.primary",
+                        display: "contents",
+                        alignItems: "center",
+                        fontWeight: 600,
+                      }}
+                    >{("Explore").toLowerCase()} ARDC {toggleBtn ? <ChevronUp  strokeWidth={3} absoluteStrokeWidth style={{marginLeft: "4px", fontWeight: 600}} size={20} /> : <ChevronDown  strokeWidth={3} absoluteStrokeWidth style={{marginLeft: "4px"}} size={20} />}</Typography>
                 </Button>
             </Stack>
         </Paper>
