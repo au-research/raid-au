4d1f697440aa1891f52beaf44ad73530ee12e3ec
Meine Toonen <meine.toonen@surf.nl>
2026-05-18 15:50:07 +0200

    Add lombok constructors to orcidname so tests pass

--- api-svc/raid-api/src/main/java/au/org/raid/api/dto/orcid/OrcidName.java
+++ api-svc/raid-api/src/main/java/au/org/raid/api/dto/orcid/OrcidName.java
@@ -2,12 +2,16 @@ package au.org.raid.api.dto.orcid;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrcidName {
     @JsonProperty("given-names")
     private OrcidStringValue givenNames;
